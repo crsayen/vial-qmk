1992ad881c0a241dfa6c18e84ba7e4124f65aa03
crsayen <crsayen@gmail.com>
2022-10-23 14:50:20 -0400

    add support for humla

---
+++ keyboards/humla/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xDD, 0x61, 0x4D, 0x12, 0x79, 0x0C, 0xCC, 0xE2}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
